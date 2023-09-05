2f69f75eae0d558585a71cc65ba0237caca7b724
yuanzhihai <396751927@qq.com>
2023-09-05 13:06:24 +0800

    init

---
+++ example/20230905140550_test.php
@@ -0,0 +1,37 @@
+<?php
+
+use think\migration\Migrator;
+use yzh52521\migrate\Schema;
+use yzh52521\migrate\Blueprint;
+
+class Test extends Migrator
+{
+
+    public function up()
+    {
+        Schema::create($this->getAdapter(), 'test', function (Blueprint $blueprint) {
+            $blueprint->id();
+            $blueprint->string('username', 64)->comment('用户名');
+            $blueprint->string('password', 255)->comment('登录密码');
+            $blueprint->integer('integer', false, true);
+            $blueprint->integer('integer2', false, false);
+            $blueprint->decimal('money', 11, 2);
+            $blueprint->decimal('money2', 10, 2, true);
+
+            $blueprint->string('avatar', 255)->comment('头像');
+            $blueprint->tinyInteger('sex')->default(0);
+            $blueprint->bigInteger('bigInt')->comment('大整数');
+            $blueprint->date('date')->nullable();
+            $blueprint->timestamps();
+
+            $blueprint->uuid();
+
+            $blueprint->index(['username', 'sex']);
+        }, ['comment' => '表注释']);
+    }
+
+    public function down()
+    {
+        $this->dropTable('test');
+    }
+}
